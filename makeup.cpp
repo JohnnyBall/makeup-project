1c71e22166d05b23fb74fe07906c825234c5ce51
John Ball <johneball93@gmail.com>
2016-06-03 10:12:54 -0400

    SetupCommandLineInterface
    Made it so it can read the commands from the command line.

--- makeup.cpp
+++ makeup.cpp
@@ -14,6 +14,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <string.h>
 using namespace std;
 
 //==============================================================================================================================
@@ -21,6 +22,47 @@ int main(int argc, char* argv[])
 {
   ifstream     dataFile;
   string       tempString;
+
+  string       fileName;
+  int          numHiddenNodes;
+  int          epoch;
+  double       learningRate;
+
+  cout<<"ARGC: "<<argc<<endl;
+  if(argc < 9)
+  {
+    cout<<"PLEASE ENTER ALL DATA NEEDED IE: FILE NAME, #Hidden nodes, Epoch, and learning rate."<<endl;
+    cout<<"Program will now close. Please Restart and try again."<<endl;
+    exit(1);
+  }
+
+  for(int i = 1; i < argc; i = i+2)
+  {
+    cout<<"argv[i]: "<<argv[i]<<endl;
+    if(strcmp(argv[i], "-f") == 0)
+    {
+      cout<<"SETTING FILE NAME"<<endl;
+      fileName = argv[i+1];
+    }
+    else if(strcmp(argv[i], "-h") == 0)
+    {
+      cout<<"SETTING NUMBER OF HIDDEN NODES"<<endl;
+      numHiddenNodes = atoi(argv[i+1]);
+    }
+    else if(strcmp(argv[i], "-e") == 0)
+    {
+      cout<<"SETTING EPOCH"<<endl;
+      epoch = atoi(argv[i+1]);
+    }
+    else if(strcmp(argv[i], "-l") == 0)
+    {
+      cout<<"SETTING learningRate"<<endl;
+      learningRate = atof(argv[i+1]);
+    }
+  }
+
+  cout<<"fileName: "<<fileName<<" numHiddenNodes:"<<numHiddenNodes<<" epoch:"<<epoch<<" learningRate:"<<learningRate<<endl;
+
   return 0;
 }  // END OF MAIN
 //==============================================================================================================================
