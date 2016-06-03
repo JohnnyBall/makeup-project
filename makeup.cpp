ca9ea1aee1359418993886cfdc37adb21b1a85ec
John Ball <johneball93@gmail.com>
2016-06-03 08:52:22 -0400

    Makeup Project Start
    Starting this makeup project I have to do yay.

---
+++ makeup.cpp
@@ -0,0 +1,26 @@
+// Name:      John Ball
+// File Name: makeup.cpp
+// Date:      2 JUNE, 2016
+//
+//
+//
+//
+//
+//
+//
+//
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+//==============================================================================================================================
+int main(int argc, char* argv[])
+{
+  ifstream     dataFile;
+  string       tempString;
+  return 0;
+}  // END OF MAIN
+//==============================================================================================================================
